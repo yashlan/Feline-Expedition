514c4179cd44dd80add44ed73759ec8af6c6c742
yashlan <yashlan007@gmail.com>
2021-12-15 19:24:43 +0700

    update

--- Assets/Scripts/Envirotment/TransitionArea.cs
+++ Assets/Scripts/Envirotment/TransitionArea.cs
@@ -5,20 +5,10 @@ public class TransitionArea : MonoBehaviour
 {
     public string Destination;
 
-    [System.Obsolete]
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            foreach (var scene in SceneManager.GetAllScenes())
-            {
-                if(scene.name != Destination)
-                {
-                    Debug.LogWarning($"Scene dengan nama : {Destination} tidak ada, silakan cek lagi");
-                    return;
-                }
-            }
-
             SceneManager.LoadScene(Destination);
         }
     }
